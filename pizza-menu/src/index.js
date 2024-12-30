a26e1ae3279ca61d2e989e6e56dc96c10d069187
Shubhasish Bhunia <shubhasishbhunia@Shubhasishs-MacBook-Air.local>
2024-12-31 02:05:19 +0530

    pizza store update

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,12 +1,96 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
+}
+
+function Pizza() {
+    return <div>
+        <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+        <h2>Pizza Spinaci</h2>
+        <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+}
+
+function Header() {
+    return <h1>Fast React Pizza Co.</h1>
+}
+function Menu() {
+    return (
+        <div>
+            <h2>Our menu</h2>
+            <Pizza />
+        </div>
+    )
+}
+
+function Footer() {
+    // const hour = new Date().getHours();
+    // const openHour = 12;
+    // const closeHour = 22;
+
+    // if (hour >= openHour && hour <= closeHour) alert("We are currently open!"); else alert("Sorry we're closed")
+
+    return <footer>{new Date().toLocaleTimeString()} We are currently open!</footer>
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+</React.StrictMode>)
 
+// Before React v18
+// React.render(<App />, document.getElementById("root"));
